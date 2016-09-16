c9d974cb2f55bfb9485f0b4aec76b6ce3e1c22da
pomochieng <pomochieng@gmail.com>
2016-09-16 18:32:20 +0000

    atm reloaded

--- atm.rb
+++ atm.rb
@@ -1,5 +1,6 @@
 attempts = 0
-while attempts <= 2
+
+while attempts <= 0
 	puts "Enter pin"
 	pin = gets.chomp
 	if pin == "1234"
@@ -9,3 +10,31 @@ while attempts <= 2
 	end
 	attempts = attempts + 1
 end
+def get_balance
+	balance = 1000
+	#puts "Your balance is....."
+	#puts balance
+	#puts "Thanks for banking with us"       #check balance	
+	"carol"
+	balance
+end
+
+def withdraw_money(amount) 			#amount is parameter when define from argument.
+	pesa = get_balance
+	puts pesa
+	balance = pesa - amount     #Withdraw
+	puts "Your current balance is....."
+	puts balance
+	puts "Thank you for banking with us."
+end
+
+
+def deposit(cash)
+	pesa = get_balance
+	puts pesa
+	balance = pesa + cash    #deposit	
+end
+
+#show_balance
+withdraw_money (600)       #(600) is an argument
+#deposit (5000)
