4d6674f8c52a2c570befeb05aec590a04fdf46b2
pomochieng <pomochieng@gmail.com>
2016-09-17 16:53:39 +0000

    Calculate Leap year

--- calcleapyear.rb
+++ calcleapyear.rb
@@ -7,13 +7,17 @@ puts 'Enter End Year Now and Press Enter Key'
 endyear = gets.chomp
 puts 'Calculating Range..........'
 puts "The Range is #{startyear} - #{endyear}"
+year = endyear.to_i - startyear.to_i   # compute the difference between the entries
 #Leap Year divisible by 4 (Calculate the years between start and end year if divisible by 4)
-startyear.upto (endyear) do  |leap|
-	if n%4 == startyear.endyear.to_i
-		puts "Leap Year"
+startyear.to_i.upto (endyear.to_i) do  |n|
+	if n%4 == 0
+		puts "#{n} - Is a Leap Year"
 	else
-		puts leap
+		puts "#{n} - - Is not a Leap Year"
 
 	end
-			
-#Diplay the results - Year divisible by 100 are not leap year unless divisible by 400.
+
+	unless n%100 && n%400               #Diplay the results - Year divisible by 100 are not leap year unless divisible by 400.
+		puts "#{n} - is a leap year"		
+	end
+	end         
