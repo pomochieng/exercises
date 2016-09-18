1ab98dd981df9c9a62121fb684712fff2d6697fc
pomochieng <pomochieng@gmail.com>
2016-09-18 23:57:58 +0000

    Add name then sort in an Array

---
+++ classmates.rb
@@ -0,0 +1,11 @@
+#Enter name.
+#puts "Please Enter name"
+
+name = ["Mercy", "Mancini", "Ian", "Wicky", "Jane", "Mose", "Sharon", "Carol", "Clive", "Peter"]
+
+puts "The sorted list is as follows:"
+
+
+puts name.sort
+
+#Sort them alphabetically
