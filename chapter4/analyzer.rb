0a4879408b26e4a7e3352dd3ef1f53478e79847d
pomochieng <pomochieng@gmail.com>
2016-09-19 01:25:27 +0000

    Add word line to count

--- chapter4/analyzer.rb
+++ chapter4/analyzer.rb
@@ -1,3 +1,7 @@
+text = ""
 line_count = 0
-File.open("text.txt").each { |line| line_count += 1 }    #Load text files on treminal
-puts line_count
+File.open("text.txt").each do |line|
+	line_count += 1
+	text << line	
+end
+puts "#{line_count}line"
