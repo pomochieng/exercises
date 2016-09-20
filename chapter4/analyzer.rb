7ea87b299ae333199160222a3dc76903f8eaa448
pomochieng <pomochieng@gmail.com>
2016-09-20 11:17:21 +0000

    analyzer

--- chapter4/analyzer.rb
+++ chapter4/analyzer.rb
@@ -4,5 +4,16 @@ File.open("text.txt").each do |line|
 	line_count += 1
 	text << line	
 end
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
 
 puts "#{line_count} lines"
+
+#counting Characters
+total_characters = text_length
+puts "#{total_characters} characters"
+
+total_characters_nospaces = text.gsub(/\s+/, '').length
+
+puts "#{total_characters_nospaces} characters excluding spaces"
