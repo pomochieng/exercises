22708891c806e7a086ff943157115859770481e6
pomochieng <pomochieng@gmail.com>
2016-09-24 18:32:10 +0000

    Percentage % of useful words

--- chapter4/analyzer.rb
+++ chapter4/analyzer.rb
@@ -6,6 +6,7 @@ File.open("text.txt").each do |line|
 	line_count += 1
 	text << line	
 end
+stopwords = %w{the a by on for of are with just but and to the my I has some in}
 lines = File.readlines("text.txt")
 line_count = lines.size
 text = lines.join
@@ -38,7 +39,13 @@ puts "#{sentence_count} Sentences"
 puts "#{sentence_count / paragraph_count} Sentences per paragraph (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
 
-
+#make a list of words in the text that aren't stopwords.
+all_words = text.scan(/\w+/)
+#Count them,  and work out the percenage of nono-stop words
+good_words = all_words.reject{ |word| stopwords.include?(word) }
+#aganist all words
+good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
+puts "#{good_percentage}% of words are non-fluff words"
 
 
 
