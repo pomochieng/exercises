2a0303fa5c8c6974645e35f41f3e720846e3e1ef
pomochieng <pomochieng@gmail.com>
2016-09-24 15:55:29 +0000

    Counting Characters

--- chapter4/analyzer.rb
+++ chapter4/analyzer.rb
@@ -13,8 +13,8 @@ text = lines.join
 puts "#{line_count} lines"
 
 #counting Characters
-#total_characters = text.length
-#puts "#{total_characters} characters"
+total_characters = text.length
+puts "#{total_characters} characters"
 
 #Count Characters without white spaces
 #total_characters_nospaces = text.gsub(/\s+/, '').length
