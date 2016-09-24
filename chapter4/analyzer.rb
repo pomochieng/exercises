2e0e30104469e0b7866ffa0ab8b6287171b37d95
pomochieng <pomochieng@gmail.com>
2016-09-24 15:54:11 +0000

    Count lines

--- chapter4/analyzer.rb
+++ chapter4/analyzer.rb
@@ -1,3 +1,5 @@
+
+#count lines
 text = ""
 line_count = 0
 File.open("text.txt").each do |line|
@@ -11,9 +13,16 @@ text = lines.join
 puts "#{line_count} lines"
 
 #counting Characters
-total_characters = text_length
-puts "#{total_characters} characters"
+#total_characters = text.length
+#puts "#{total_characters} characters"
 
-total_characters_nospaces = text.gsub(/\s+/, '').length
+#Count Characters without white spaces
+#total_characters_nospaces = text.gsub(/\s+/, '').length
+#puts "#{total_characters_nospaces} characters (excluding spaces)"
 
-puts "#{total_characters_nospaces} characters excluding spaces"
+
+#Counting Words
+#word_count = text.split.length
+#puts "#{word_count} words"
+
+
