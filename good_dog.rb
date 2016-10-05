49115288959c0f436b4815f602047b01bc04c0e0
pomochieng <pomochieng@gmail.com>
2016-10-05 17:40:19 +0000

    modules

--- good_dog.rb
+++ good_dog.rb
@@ -1,8 +1,26 @@
 #good_dog.rb
 
+module Speak
+  def speak(sound)
+    puts "#{sound}"
+  end
+end
+
+
 class GoodDog
+
+  include Speak
 
 end
 
-sparky = GoodDog.new
+class HumanBeing
+  include Speak
+end
+
+
+sparky= GoodDog.new
+sparky.speak("Arf!")
+bob = HumanBeing.new
+bob.speak("Hello!")
+
 
